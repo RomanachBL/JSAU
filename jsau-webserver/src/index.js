74c487660e6c0c4c33f49c41682f01403d047b85
RomanachBL <carrey.romain@gmail.com>
2018-10-18 17:09:25 +0200

    derniers commit

--- jsau-webserver/src/index.js
+++ jsau-webserver/src/index.js
@@ -13,26 +13,94 @@ app.use(bodyParser.json())
 // parse application/x-www-form-urlencoded
 app.use(bodyParser.urlencoded({ extended: false }))
 
+fs.readFile('src/data.json', function (err, data) {
+  if(data.toString() == ''){
+    fs.writeFile('src/data.json', '[]', (err) => {
+      if (err) throw err;
+    })
+  }
+})
+
 app.post('', function(req,res) {
-  fs.appendFile('src/data.json', JSON.stringify(req.body, null, 2)+'\r\n', function(err) {
-    if (err) throw err;
-    //console.log('Done !');
-  });
+  fs.readFile('src/data.json', function (err, data) {
+    data = JSON.parse(data);
+    data.push(req.body)
+    var json = JSON.stringify(data)
+    fs.writeFile('src/data.json', json+'\n\r', (err) => {
+      if (err) throw err;
+      //console.log('Done !');
+    })
+  })
   res.redirect('');
 });
 
 app.get('/stock', function(req, res){
-  let jsonData = require('../src/data.json');
-  //console.log(jsonData);
-  res.send(jsonData);
+  fs.readFile('src/data.json',function (err, data) {
+    var data = JSON.parse(data)
+    var html = "<table>"
+    html += "<tr>"
+    html += "<th>Nom</th>"
+    html += "<th>Prix</th>"
+    html += "<th>Nombre</th>"
+    html += "<th>Genre</th>"
+    html += "</tr>"
+    for (x in data){
+      html += "<tr><td>"+data[x]["nom"]+"</td>"
+      html += "<td>"+data[x]["prix"]+"</td>"
+      html += "<td>"+data[x]["nombre"]+"</td>"
+      html += "<td>"+data[x]["genre"]+"</td></tr>"
+    }
+    html += "</table>"
+    res.send(html);
+  });
 });
 
-// !!!!!!!!!!!!!!!!!!!!! PROBLEME => LE FICHIER JSON S'ECRIT MAL POUR PLUSIEURS OBJETS (MAIS BON POUR UN SEUL OBJET)
+app.get('/stock/:genre', function (req, res) {
+  fs.readFile('src/data.json', function (err, data) {
+    var jeux = JSON.parse(data)
+    var liste_genres = []
+    for (x in jeux){
+      if (jeux[x]["genre"] == req.params.genre){
+        liste_genres.push(jeux[x])
+      }
+    }
+    res.json(liste_genres)
+  });
+})
 
+app.get('/:nom', function (req, res) {
+  fs.readFile('src/data.json', function (err, data) {
+    var nom = JSON.parse(data)
+    var liste_noms = []
+    for (x in nom){
+      if (nom[x]["nom"] == req.params.nom){
+        liste_noms.push(nom[x])
+      }
+    }
+    res.json(liste_noms)
+  });
+})
+
+app.delete('/delete/:nom', function (req, res) {
+  fs.readFile('src/data.json', function (err, data) {
+    var nom = JSON.parse(data)
+    for (var x in nom){
+      if (nom[x]["nom"] == req.params.nom){
+        delete nom[x]
+      }
+    }
+    fs.writeFile('src/data.json', JSON.stringify(data.filter(x => x)), (err) => {
+      if (err) throw err;
+      //console.log('Done !');
+    })
+    res.redirect('');
+  });
+})
+
+//modifier les méthodes d'affichage (HTML et non JSON)
+//methode override
+// FETCH DELETE, PUT etc ...
+//browserify
+//CSS d3?
 
 app.listen(8083)
-
-/*
-TODO -> Bouton sur l'interface "voir ci, voir ça " etc ...
-Utiliser le tuto du prof pour utiliser les méthodes get pour afficher des choses spécifiques
-*/
